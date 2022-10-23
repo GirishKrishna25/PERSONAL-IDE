cd584308439a1ebeafd51b2a588908522ab0367e
Girish <i.girish.krishna@gmail.com>
2022-10-23 18:38:21 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+npm i styled-components
+npm i --save-dev @types/styled-components
+
+npm install react-icons
+
+npm install react-select
+
+npm install uuid
+
+npm install react-router-dom
+
+npm install buffer
+
+npm install axios
+
+npm install codemirror
+npm install @uiw/react-codemirror
+-- themes
+npm install @uiw/codemirror-theme-duotone
+npm install @uiw/codemirror-theme-xcode
+npm install @uiw/codemirror-theme-okaidia
+npm install @uiw/codemirror-theme-github
+npm install @uiw/codemirror-theme-darcula
+npm install @uiw/codemirror-theme-bespin
+-- languages
+npm install @codemirror/lang-cpp
+npm install @codemirror/lang-java
+npm install @codemirror/lang-javascript
+npm install @codemirror/lang-python
---
+++ src/components/modalTypes/NewCard.tsx
@@ -0,0 +1,91 @@
+import React, { useState, useContext } from "react";
+import { RiCloseFill } from "react-icons/ri";
+import { PlaygroundContext } from "../../context/PlaygroundContext";
+import { CloseButton, Header, Input, ModalProps } from "../Modal";
+import Select from "react-select";
+import styled from "styled-components";
+
+const NewCard = ({ closeModal, identifer }: ModalProps) => {
+  const { folderId } = identifer;
+
+  // access our card from folders state
+  const { folders, createNewPlayground } = useContext(PlaygroundContext)!;
+
+  const languageOptions = [
+    { value: "c++", label: "C++" },
+    { value: "java", label: "Java" },
+    { value: "javascript", label: "JavaScript" },
+    { value: "python", label: "Python" },
+  ];
+
+  const [title, setTitle] = useState("");
+  const [language, setLanguage] = useState(languageOptions[0]);
+
+  const handleLanguageChange = (selectedOption: any) => {
+    setLanguage(selectedOption);
+  };
+
+  return (
+    <div>
+      <Header>
+        <h2>Create New Playground</h2>
+        <CloseButton
+          onClick={() => {
+            closeModal();
+          }}
+        >
+          <RiCloseFill />
+        </CloseButton>
+      </Header>
+      <InputWithSelect>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          autoFocus
+        />
+        <Select
+          options={languageOptions}
+          value={language}
+          onChange={handleLanguageChange}
+        />
+        <button
+          onClick={() => {
+            createNewPlayground(folderId, title, language.value);
+            closeModal();
+          }}
+        >
+          Create Playground
+        </button>
+      </InputWithSelect>
+    </div>
+  );
+};
+
+export default NewCard;
+
+const InputWithSelect = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 0.5fr;
+  row-gap: 1rem;
+  column-gap: 1rem;
+  margin-top: 1.2rem;
+  align-items: center;
+
+  input {
+    flex-grow: 1;
+    height: 2rem;
+    padding: 0.5rem;
+    border-radius: 5px;
+  }
+
+  button {
+    background: #241f21;
+    height: 2rem;
+    color: white;
+    padding: 0 2rem;
+    border-radius: 5px;
+  }
+`;
